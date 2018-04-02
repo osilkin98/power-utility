f2b2e547a4272855c774d753ce9a4b2cbe788a83
Oleg Silkin <o.silkin98@gmail.com>
2018-04-02 18:31:38 -0500

    Add files via upload
    added removal scripts for after install

--- install.sh
+++ install.sh
@@ -7,6 +7,21 @@
 
 # to move the executable to /usr/bin
 su -c 'cp power /usr/bin/power'
+stat="$?"
+if [ $stat = "0" ]; then
+    rm power
+    # echo "Successfully copied power to /usr/bin/, status code ${stat}"
+else
+    echo "There was an issue with copying `power` to `/usr/bin/`, status code ${stat}"
+fi
 
 # to move the information file into the correct folder
 su -c 'cp power.help /usr/share/power/power.help'
+stat="$?"
+if [ $stat = "0" ]; then
+    rm "power.help"
+    # echo "Removed power.help, status code ${stat}"
+else
+    echo "There was an issue copying power.help to /usr/share/power/, status code ${stat}"
+fi
+
